545c9c124b62ea063266354bf38811a46e48f8e0
Byt3 <205er@users.noreply.github.com>
2016-12-28 15:31:59 +0100

    Add files via upload

---
+++ files/www/lan/update.js
@@ -0,0 +1,24 @@
+
+function init() {
+	/* Nothing to do */
+}
+
+function restore_firmware() {
+	if(!confirm("Sollen alle Einstellungen zur\xFCckgesetzt werden?")) return;
+	send("/cgi-bin/upgrade", { func : 'restore_firmware' }, function(text) {
+		setText('msg', text);
+	});
+}
+
+function lookup_upgrade() {
+	send("/cgi-bin/upgrade", { func : 'lookup_upgrade' }, function(text) {
+		setText('msg', text);
+	});
+}
+
+function lookup_and_apply_upgrade() {
+	if(!confirm("Soll ein Update durchgef\xFChrt werden?")) return;
+	send("/cgi-bin/upgrade", { func : 'lookup_and_apply_upgrade' }, function(text) {
+		setText('msg', text);
+	});
+}
