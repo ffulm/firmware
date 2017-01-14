3df102632caea0dfb34bfeb210117361a0892c9c
L3D - Lilian Roller <l3d@see-base.de>
2017-01-14 21:18:56 +0100

    [ffulm] change code style slightly

--- files/www/lan/home.js
+++ files/www/lan/home.js
@@ -1,5 +1,6 @@
+
 function formatSize(bytes) {
-	if(typeof bytes === "undefined" || bytes == "") {
+	if (typeof bytes === "undefined" || bytes == "") {
 		return "-";
 	} else if (bytes < 1000) {
 		return bytes + "  B";
@@ -20,25 +21,25 @@ function formatSpeed(bytes) {
 function init() {
 	send("/cgi-bin/home", { }, function(data) {
 		var obj = fromUCI(data).misc.data;
-		for(var key in obj) {
+		for (var key in obj) {
 			var value = obj[key];
 
-			if(key == 'stype') {
+			if (key == 'stype') {
 				continue;
 			}
 
 			// for data volume
-			if(key.endsWith("_data")) {
+			if (key.endsWith("_data")) {
 				value = formatSize(value);
 			}
 
 			// for transfer speed
-			if(key.endsWith("_speed")) {
+			if (key.endsWith("_speed")) {
 				value = formatSpeed(value);
 			}
-			
+
 			//for addresses
-			if(typeof(value) == 'object') {
+			if (typeof(value) == 'object') {
 				value = "<ul><li>"+value.join("</li><li>")+"</li></ul>"
 			}
 
@@ -53,5 +54,5 @@ function init() {
 	addHelpText($("software"), "Einige installierte Softwareversionen.");
 	addHelpText($("freifunk_user_count"), "Die Anzahl der Nutzer an diesem Router in den letzten zwei Stunden.");
 	addHelpText($("lan_user_count"), "Die Anzahl der Nutzer an diesem Router in den letzten zwei Stunden.");
-	addHelpText($("has_vpn"), "Der VPN-Server im Internet, mit dem der Knoten verbunden ist.");
+	addHelpText($("vpn_server"), "Der VPN-Server im Internet, mit dem der Knoten verbunden ist.");
 }
