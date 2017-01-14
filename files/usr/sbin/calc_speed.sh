ac59426379178ca7500c307623b688c583134741
L3D - Lilian Roller <l3d@see-base.de>
2017-01-14 20:51:47 +0100

    [ffulm] calculate up/down speed for interfaces br-lan, br-wan and br-freifunk

--- files/usr/sbin/calc_speed.sh
+++ files/usr/sbin/calc_speed.sh
@@ -1,25 +1,19 @@
 #!/bin/sh                                                                                              
 
-# Calculate up/down speed and record data volume on given network interfaces.
+# Calculate up/dpwn speed on given network interfaces
 
-net="$1"
-td="$2"
+for net in $@; do
+        for act in rx_bytes tx_bytes; do
+                bytes1="$(cat /var/$act$net)"
+                time1="$(date +%s -r /var/$act$net)"
 
-if [ -n "$net" -a -n "$td" ]; then
-	bytes1="$(cat /var/${td}_data_${net} 2> /dev/null)"
-	time1="$(date +%s -r /var/${td}_data_${net} 2> /dev/null)"
+                bytes2="$(cat /sys/class/net/$net/statistics/$act)"
+                time2="$(date +%s)"
 
-	bytes2="$(cat /sys/class/net/$net/statistics/${td}_bytes)"
-	time2="$(date +%s)"
+                speed="$(expr \( $bytes2 - $bytes1 \) / \( $time2 - $time1 \) 2> /dev/null)"
+                echo "${speed:-0}" > /var/speed_$act$net
 
-	# Only calculate new speed after at least one second has passed
-	if [ "$time1" != "$time2" ]; then
-		# Remember data volume for next time
-		echo -n "$bytes2" > /var/${td}_data_${net}
-	fi
-
-	speed="$(expr \( $bytes2 - $bytes1 \) / \( $time2 - $time1 \) 2> /dev/null)"
-	echo "${speed:-0}"
-else
-	echo "Usage: $0 <interface> [tx|rx]"
-fi
+                # Remember bytes for next time
+                echo -n "$bytes2" > /var/$act$net
+        done
+done
